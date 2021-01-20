7b22e607ed062e66d01864af1d86675d5849a858
Md Hosne Mobarok <59361903+hosnemobarok@users.noreply.github.com>
2021-01-20 21:58:40 +0600

    Update 20_Perfect Numbers.py

--- Codding Interview/1--Mathematical/20_Perfect Numbers.py
+++ Codding Interview/1--Mathematical/20_Perfect Numbers.py
@@ -2,13 +2,13 @@
     Time Complexity     : O(sqrt(N))
     Space Complexity    : O(1)
 '''
-from math import sqrt
 def Perfect_Number(number):
-    if number == 1:
+    if number <= 1:
         return 0
 
     sum = 1
-    for i in range(2, int(sqrt(number)+1)):
+    sqrt = int(number ** 0.5)
+    for i in range(2, sqrt+1):
         if number % i == 0:
             sum += (i + number // i)
 
@@ -26,26 +26,23 @@ if __name__ == '__main__':
 
 '''
 # User function Template for python3
-from math import sqrt
-
 
 class Solution:
     def isPerfectNumber(self, N):
         # code here
-        if N == 1:
+        if N <= 1:
             return 0
-
+            
         sum = 1
-        #              2,  (N ** 0.5)
-        for i in range(2, int(sqrt(N) + 1)):
+        sqrt = int(N ** 0.5)
+        for i in range(2, sqrt+1):
             if N % i == 0:
                 sum += (i + N // i)
-
-        if N == sum:
+                
+        if sum == N:
             return 1
         else:
             return 0
-
 # {
 #  Driver Code Starts
 # Initial Template for Python 3
