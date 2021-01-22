d5e31108288f55d9880394eedd185f278175427b
Md Hosne Mobarok <59361903+hosnemobarok@users.noreply.github.com>
2021-01-22 21:17:27 +0600

    Update p49 What is basic.py

--- 2-Dimik oj/p49 What is basic.py
+++ 2-Dimik oj/p49 What is basic.py
@@ -1,56 +1,27 @@
-#Dimikoj NOt Accept cesta korte hobe
-
-T = int(input())
-for i in range(T):
-    N = int(input())
-    if N > 1:
-        for j in range(2,N):
-            if N % j == 0:
-                print("%d is not a prime"%N)
-                break
-        else:
-            print("%d is a prime"%N)
-
-
-
-#2nd Niom Dimikoj Accept
-T = int(input())
-for i in range(T):
-    N = int(input())
-    if N == 2:
-        print("%d is a prime" % (N))
-    else:
-        a = []
-        z = int(N ** (1 / 2))
-        for f in range(2, z + 1):
-            if N % f == 0:
-                a.append(True)
-
-        if any(a):
-            print("%d is not a prime" % (N))
-        else:
-            print("%d is a prime" % (N))
-
-
-
-#3rd niom helal vi Dimikoj Accept
-def prime(num):
-    import math
-    if num == 2:
-        return True
-    if num%2 == 0:
+def is_Prime(n):
+    if n <= 1:
         return False
-    for i in range(3, int(math.sqrt(num))+1, 2):
-        if num % i == 0:
+    if n == 2:
+        return True
+
+    # 2 skip all even number is'n prime.
+    if n > 2 and n % 2 == 0:
+        return False
+
+    root = int((n ** 0.5))
+
+    for i in range(3, root+1, 2):
+        if n % i == 0:
             return False
+
     return True
 
-
-if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-        num = int(input())
-        if prime(num):
-            print("%d is a prime"%num)
+# Driver Code
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        n = int(input())
+        res = is_Prime(n)
+        if res:
+            print("%d is a prime"%n)
         else:
-            print("%d is not a prime"%num)
+            print("%d is not a prime" % n)
