fc014d57d0587f74a4851dee0aaedac9649dc362
Md Hosne Mobarok <59361903+hosnemobarok@users.noreply.github.com>
2021-02-13 23:52:09 +0600

    Delete 5--LeetCode directory

--- 5--LeetCode/3--Array/1185. Day of the Week.py
+++
@@ -1,6 +0,0 @@
-import datetime
-class Solution:
-    def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
-        return datetime.date(year, month, day).strftime("%A")
-
-
--- 5--LeetCode/4--String/1678. Goal Parser Interpretation.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def interpret(self, command: str) -> str:
-        a = command.replace('()', 'o')
-        return a.replace('(al)', 'al')
