653e9bab6f2050f9bf4d5ca85d9ce2621347e2e9
Md Hosne Mobarok <59361903+hosnemobarok@users.noreply.github.com>
2021-04-10 00:56:32 +0600

    Delete 5--LeetCode directory

--- 5--LeetCode/2 - Medium/287. Find the Duplicate Number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        res = []
-        for i in nums:
-            if i not in res:
-                res.append(i)
-            else:
-                return i
