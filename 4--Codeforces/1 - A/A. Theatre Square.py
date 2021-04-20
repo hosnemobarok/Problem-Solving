dc036f4a353cf5f9603572d7a4c12aa506576938
Md Hosne Mobarok <59361903+hosnemobarok@users.noreply.github.com>
2021-04-20 13:56:59 +0600

    Add files via upload

---
+++ 4--Codeforces/1 - A/A. Theatre Square.py
@@ -0,0 +1,17 @@
+from math import ceil
+
+
+def Theatre_Square(n, m, a):
+    _n = ceil(n / a)
+    _m = ceil(m / a)
+
+    """print(_n)
+    print(_m)"""
+
+    return _n * _m
+
+
+if __name__ == '__main__':
+    n, m, a = map(int, input().split())
+    res = Theatre_Square(n, m, a)
+    print(res)
