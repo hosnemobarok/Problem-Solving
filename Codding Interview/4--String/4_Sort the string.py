0ac8d8783713c4abbbe89aed16e7039de6fa8943
Md Hosne Mobarok <59361903+hosnemobarok@users.noreply.github.com>
2021-08-10 01:17:45 +0600

    Delete Codding Interview directory

--- Codding Interview/4--String/4_Sort the string.py
+++
@@ -1,9 +0,0 @@
-def string_sort(string):
-    res = sorted(string)
-    print(*res[::-1], sep='')
-
-
-if __name__ == '__main__':
-    for _ in range(int(input())):
-        string = input()
-        string_sort(string)
