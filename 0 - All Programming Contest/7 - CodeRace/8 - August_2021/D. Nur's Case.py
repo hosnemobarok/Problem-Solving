0d7b3798bee8e31dad77a385feeef5e2011206cf
Md Hosne Mobarok <59361903+hosnemobarok@users.noreply.github.com>
2021-09-03 20:46:05 +0600

    Add files via upload

---
+++ 0 - All Programming Contest/7 - CodeRace/8 - August_2021/D. Nur's Case.py
@@ -0,0 +1,21 @@
+
+main_web = input()
+
+ok_web = main_web.replace("j", "l").replace("!", "l").replace("@", "a").replace("O", "a").replace("o", "a").replace("D", "a").replace("0", "a").replace("e", "a").replace("S", "s").replace("5", "s").replace("2", "s").replace("i", "1").replace("?", "1")
+count = 0
+ok_web_position = []
+for ind in range(int(input())):
+    user_web = input()
+    temp_user_web = user_web.replace("j", "l").replace("!", "l").replace("@", "a").replace("O", "a").replace("o", "a").replace("D", "a").replace("0", "a").replace("e", "a").replace("S", "s").replace("5", "s").replace("2", "s").replace("i", "1").replace("?", "1")
+
+
+    if ok_web != main_web and user_web == ok_web:
+        ok_web_position.append(ind + 1)
+        count += 1
+
+    if temp_user_web == ok_web:
+        ok_web_position.append(ind +1)
+        count += 1
+
+print(count)
+print(*ok_web_position)
