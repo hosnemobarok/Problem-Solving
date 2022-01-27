1cb8751758ab3ec5e5bfb164f10211d7f98f5399
Md Hosne Mobarok <59361903+hosnemobarok@users.noreply.github.com>
2022-01-27 22:18:36 +0600

    Add files via upload

---
+++ 5 - LeetCode/1 - Esay/FizzBuzz.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func fizzBuzz(_ n: Int) -> [String] {
+        
+        var res:[String] = []
+        for i in 1...n {
+            if (i % 3 == 0) && (i % 5 == 0){
+                res.append("FizzBuzz")
+                
+            } else if (i % 3 == 0){
+                res.append("Fizz")
+                
+            }else if (i % 5 == 0){
+                res.append("Buzz")
+            }else{
+                res.append(String(i))
+            }
+        }
+        return res
+    }
+}
