bc782d082741e311d87cf2dfdaf457534d765146
Md Hosne Mobarok <59361903+hosnemobarok@users.noreply.github.com>
2022-02-27 01:14:56 +0600

    Intra DUET Programming Mock Contest Beginner-2022

---
+++ 0 - All Programming Contest/0 - All ICPC and NCPC and Other's Contest/5 - IntraDUETProgrammingMock ContestBeginner/inter_DUET_Programming_Contest.py
@@ -0,0 +1,20 @@
+# Contest link: --> https://toph.co/arena?practice=621a74bcbb37c55277078c41#!/dashboard
+ 	
+ def aSolution():
+ 	for i in range(int(input())):
+		x, y = map(int, input().split())
+	
+		xx = pow(x, 2)
+		yy = pow(y, 2)
+		
+		print(xx - yy)
+
+
+def bSolution():
+	print("Thanks to DUETCS.")
+	
+	
+
+# Driver
+ aSolution()
+ bSolution()
